18e77f2dfb9ffccd17917d7e6c7323f854039c00
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-04-16 15:16:04 +0700

    Add 'tuple' and 'unit' tests in main.rs
    Two new test functions 'tuple' and 'unit' have been added to main.rs. The 'tuple' function tests tuple operations like assignment and printing while 'unit' function involves basic testing of units. This enhances our test coverage for these specific data types.

--- src/main.rs
+++ src/main.rs
@@ -148,3 +148,34 @@ fn char_type(){
     println!("{} {}", char1, char2);
 }
 
+
+#[test]
+fn tuple(){
+    let mut data: (i32, f64, bool) = (10, 10.5, true);
+    println!("{:?}", data);
+
+    // let a = data.0;
+    // let b = data.1;
+    // let c = data.2;
+
+    let (a, b, c) = data;
+    println!("{} {} {}", a, b, c);
+
+    data.0 = 20;
+    data.1 = 20.5;
+    data.2 = false;
+    println!("{:?}", data);
+}
+
+fn unit(){
+    println!("Hello");
+}
+
+#[test]
+fn test_unit(){
+    let result: () = unit();
+    println!("{:?}", result);
+
+    let test: () = ();
+    println!("{:?}", test);
+}
