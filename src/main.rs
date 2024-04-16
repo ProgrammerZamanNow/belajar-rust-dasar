25762ff3a9fb6c2476205f6c92901a0d7454e5c4
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-04-16 14:20:37 +0700

    The commit message seems wrong based on the presented diff. The correct one would probably be:
    Add variable-related test functions in main.rs
    
    Several new test functions regarding variable usage were added in main.rs. These test functions cover different aspects of variable usage, like mutability, static typing, and shadowing. The aim is to ensure that these features within the Rust programming language are working properly.

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,36 @@ fn main() {
 fn hello_test(){
     println!("Hello Test");
 }
+
+#[test]
+fn test_variable(){
+    let name = "Eko Kurniawan Khannedy";
+    println!("Hello {}", name);
+}
+
+#[test]
+fn test_mutable(){
+    let mut name = "Eko Kurniawan Khannedy";
+    println!("Hello {}", name);
+
+    name = "Budi Nugraha";
+    println!("Hello {}", name);
+}
+
+#[test]
+fn static_typing(){
+    let name = "Eko Kurniawan Khannedy";
+    println!("Hello {}", name);
+
+    // name = 10;
+    println!("Hello {}", name);
+}
+
+#[test]
+fn shadowing(){
+    let name = "Eko Kurniawan Khannedy";
+    println!("Hello {}", name);
+
+    let name = 10;
+    println!("Hello {}", name);
+}
