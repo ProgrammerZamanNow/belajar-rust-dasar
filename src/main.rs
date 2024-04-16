7c0d2ccbdad16fc8bb2a86718c35b22baaa2f3e3
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-04-16 15:00:58 +0700

    Add 'boolean_operator' test in main.rs
    A new test function 'boolean_operator' has been added to main.rs. The function tests the boolean operations using the variables 'absen' and 'nilai_akhir'. It evaluates if the specified conditions are met and outputs if these conditions resulted in passing the test. Thus, improving the test coverage for boolean operations.

--- src/main.rs
+++ src/main.rs
@@ -127,3 +127,15 @@ fn comparison(){
     let result : bool = a >= b;
     println!("{}", result);
 }
+
+#[test]
+fn boolean_operator(){
+    let absen = 75;
+    let nilai_akhir = 80;
+
+    let lulus_absen: bool = absen >= 75;
+    let lulus_nilai_akhir: bool = nilai_akhir >= 75;
+
+    let lulus: bool = lulus_absen && lulus_nilai_akhir;
+    println!("{}", lulus);
+}
