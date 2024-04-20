03386b94c62bb7b3fb98723dfb4285ace8e5f503
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-04-20 22:01:42 +0700

    Implemented lifetime annotations and related tests
    This commit introduces lifetime annotations to functions and structs in the main program file. It also adds a variety of tests to ensure correct handling of references and lifetimes, including testing for dangling references. This enhances code readability and enables more effective memory management.

--- src/main.rs
+++ src/main.rs
@@ -1521,3 +1521,97 @@ fn test_recoverable_error() {
         }
     }
 }
+
+#[test]
+fn test_dangling_reference() {
+    let r: &i32;
+    {
+        let x = 5;
+        // r = &x; error
+    }
+    r = &40;
+    println!("{}", r)
+}
+
+fn longest<'a>(value1: &'a str, value2: &'a str) -> &'a str {
+    if value1.len() > value2.len() {
+        value1
+    } else {
+        value2
+    }
+}
+
+#[test]
+fn test_lifetime_annotation() {
+    let value1 = "Eko";
+    let value2 = "Kurniawan";
+    let result = longest(value1, value2);
+    println!("{}", result);
+}
+
+#[test]
+fn test_lifetime_annotation_dangling_reference() {
+    let string1 = String::from("Khannedy");
+    let string2 = String::from("Eko");
+    let result;
+    {
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    println!("{}", result);
+}
+
+struct Student<'a, 'b> {
+    name: &'a str,
+    last_name: &'b str,
+}
+
+impl<'a, 'b> Student<'a, 'b> {
+    fn longest_name(&self, student: &Student<'a, 'b>) -> &'a str {
+        if self.name.len() > student.name.len() {
+            self.name
+        } else {
+            student.name
+        }
+    }
+}
+
+fn longest_student_name<'a, 'b>(student1: &Student<'a, 'b>, student2: &Student<'a, 'b>) -> &'a str {
+    if student1.name.len() > student2.name.len() {
+        student1.name
+    } else {
+        student2.name
+    }
+}
+
+#[test]
+fn test_student() {
+    let student = Student {
+        name: "Eko",
+        last_name: "Khannedy",
+    };
+    println!("{}", student.name);
+    let student2 = Student {
+        name: "Budi",
+        last_name: "Nugraha",
+    };
+    let result = longest_student_name(&student, &student2);
+    println!("{}", result);
+
+    let result = student.longest_name(&student2);
+    println!("{}", result);
+}
+
+struct Teacher<'a, ID> where ID: Ord {
+    id: ID,
+    name: &'a str,
+}
+
+#[test]
+fn test_lifetime_annotation_generic() {
+    let teacher: Teacher<i32> = Teacher {
+        id: 10,
+        name:"Eko",
+    };
+    println!("{}", teacher.id);
+    println!("{}", teacher.name);
+}
